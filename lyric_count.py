a7e049f3e77b10eeb04c28182b86187110c98c28
stepjue <stepjue@gmail.com>
2014-11-24 23:20:27 -0800

    D3 visualization of lyric frequencies

--- lyric_count.py
+++ lyric_count.py
@@ -7,7 +7,7 @@ from pprint import pprint
 YEAR_START = 1946
 YEAR_END = 2013
 lyric_stopwords = set(["chorus", "refrain", "bridge", "x2", "x3", "x4", "x5"])
-contraction_stopwords = set(["aren", "haven", "don", "couldn", "hasn", "didn", "mightn", "mustn", "shouldn", "wouldn", "ain", "re", "ve", "t", "ll", "d", "s", "m"])
+contraction_stopwords = set(["aren", "haven", "don", "couldn", "hasn", "didn", "mightn", "mustn", "shouldn", "wouldn", "ain", "ev", "ry", "re", "ve",  "er", "y", "e", "t", "ll", "d", "s", "m"])
 english_stopwords = set(stopwords.words("english")) | contraction_stopwords
 
 def extract_words(lyric_string):
@@ -15,7 +15,7 @@ def extract_words(lyric_string):
    word_count = Counter(raw_set.difference(lyric_stopwords).difference(english_stopwords))
    return word_count
 
-lyrics_dict = {}
+lyrics = []
 for year in range(YEAR_START, YEAR_END + 1):
    counter = Counter()
    with open("billboard.txt", "r") as readfile:
@@ -29,14 +29,14 @@ for year in range(YEAR_START, YEAR_END + 1):
    # Finds actual number of songs (ignores entries with no lyrics)
    num_songs = len([entry for entry in data[str(year)] if entry.get("lyrics") != None and len(entry["lyrics"]) > 0])
 
+   #print(num_songs)
    # Normalizes word counts by dividing by number of songs
    words_normalized = [(entry[0], entry[1] / num_songs) for entry in words]
 
    # Only keep word counts that are in 15% of songs
-   top_words_normalized = [entry for entry in words_normalized if entry[1] > 0.10]
-   lyrics_dict[year] = {}
+   top_words_normalized = [entry for entry in words_normalized if entry[1] > 0.12]
    for entry in top_words_normalized:
-      lyrics_dict[year][entry[0]] = entry[1]
+      lyrics.append({"year": year, "word": entry[0], "frequency": round(entry[1], 5)})
 
 with open("lyric_count.json", 'w') as out:
-   json.dump(lyrics_dict, out)
+   json.dump(lyrics, out)
